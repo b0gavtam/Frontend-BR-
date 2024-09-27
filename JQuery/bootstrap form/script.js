b27f717c7daf77692fb5615641c97a3dfdbc54e9
Tamás Gavrán <b0gavtam@vasvari.org>
2024-09-27 08:15:50 +0200

    urlap

--- JQuery/bootstrap form/script.js
+++ JQuery/bootstrap form/script.js
@@ -1,24 +1,37 @@
 let emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 let pwdPattern = /^[\w]{8,256}$/
+let irszamPattern =  /^[0-9]{4}$/
+let errors = [];
 $(function(){ 
     $("form").submit(function(event){ 
         event.preventDefault();
     })
-    $("input").change(function(){ 
-       let email = $(this).val();
-       let type = $(this).attr("type");
-        if(!emailPattern.test(email) && type == "email"){
-            $("error").html("<p> Az email formátuma nem megfelelő! </p>")
+    $("input").change(function(){
+       
+        let email = $(this).val().trim();
+        let pwd = $(this).val().trim();
+        let irsz = $(this).val().trim();
+        let type = $(this).attr("type");
+        if (!emailPattern.test(email) && type == "email"){
+            errors.push("<p>Az email formátuma nem megfelelő!</p>");
         }
-        else{ 
-            $(".error").html("");
+        else{
+            errors = errors.filter(error => !error.includes("email"))
         }
-        let pwd = $(this).val()
-        if (!pwdPattern.test(pwd) && type == "password") {
-           $("error").html("<p> A jelszó formátuma nem megfelelő! </p>")
+        if (!pwdPattern.test(pwd) && type == "password"){
+            errors.push("<p>Az jelszó hossza nem megfelelő!</p>");
         }
-       else{ 
-           $(".error").html("");
-       }
+        else{
+            errors = errors.filter(error => !error.includes("jelszó"))
+        }
+        if (!irszPattern.test(irsz) && type == "text"){
+            errors.push("<p>Az irányítószám nem megfelelő!</p>");
+        }
+        else{
+            errors = errors.filter(error => !error.includes("irányítószám"))
+        }
+        $(".error").html(errors.join(""));
     })
+    $(".error").html(errors.join(" "));
+    console.table(errors);
 })
