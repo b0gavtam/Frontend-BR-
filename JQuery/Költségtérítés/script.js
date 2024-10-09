6e43501a013a51a0aee6318cbd1093c883f5a442
Tamás Gavrán <b0gavtam@vasvari.org>
2024-10-09 15:12:34 +0200

    FEL

--- JQuery/Költségtérítés/script.js
+++ JQuery/Költségtérítés/script.js
@@ -1,70 +1,121 @@
-$(function(){
-    let benzin =[{
-        value: "7.6",
-        text: "1000 cm3-ig"
-    },
-    {
-        value: "8.6",
-        text: "1001-1500 cm3"
-    },
-    {
-        value: "9.5",
-        text: "1501-2000 cm3"
-    },
-    {
-        value: "11.4",
-        text: "2001-3000 cm3"
-    },
-    {
-        value: "13.3",
-        text: "3000 cm3 felett"
-    }
-    ]
-    let diesel =[{
-        value: "5.7",
-        text: "1000 cm3-ig"
-    },
-    {
-        value: "6.7",
-        text: "1001-1500 cm3 alatt"
-    },
-    {
-        value: "7.6",
-        text: "1501-2000 cm3"
-    },
-    {
-        value: "9.5",
-        text: "2001-3000 cm3"
-    },
+$(function () {
+    var rendszamtest = /(^[A-Za-z]{2}-[A-Za-z]{2}-\d{3}$ | ^[A-Za-z]{3}-\d{3}$)/;
+    var egyegartest = /^\d{3}$/
+    var szamtest = /^\d$/
+    let egysegar;
+    let rendszam;
+    let megtett;
+    let honnanhova;
 
+
+    let benzin = [
+        {
+            value: '7.6',
+            text: '1000 cm3-ig'
+        },
+        {
+            value: '8.6',
+            text: '1001 - 1500 cm3-ig'
+        },
+        {
+            value: '9.5',
+            text: '1501 - 2000 cm3-ig'
+        },
+        {
+            value: '11.4',
+            text: '2001 - 3000 cm3-ig'
+        },
+        {
+            value: '13.3',
+            text: '3001 cm3 felett'
+        }
     ]
-    let filled = [false, true, true, true, true, true, true, true];
+    let diesel = [
+        {
+            value: '5.7',
+            text: '1500 cm3-ig'
+        },
+        {
+            value: '6.7',
+            text: '1501 - 2000 cm3-ig'
+        },
+        {
+            value: '7.6',
+            text: '2001 - 3000 cm3-ig'
+        },
+        {
+            value: '9.5',
+            text: '3001 cm3 felett'
+        }
+    ]
     let uzemanyag;
-    $("form").submit(function(e){
-        e.preventdefault();
+    let filled = [false, false, false, false, false, true, false, false];
+    $("form").submit(function (e) {   //Az kitöltött űrlap elemei nem kerülnek törlésre.
+        e.preventDefault();
     })
-    $("[name = hengerurtartalom]").change(function(){
+
+    $("[name=uzemanyagInput]").change(function () {
         uzemanyag = $(this).val();
-        $("#hengerurtartalom").html("<option value = '-1'></option>");
-        if(uzemanyag === "diesel"){
-            getUzemanyag(diesel)
-            //console.log(o.text);
+        filled[0] = true;
+        console.log(filled)
+        $("#cm3Input").html('<option value="-1"></option>');
+        if (uzemanyag === "diesel") {
+            getUzemanyag(diesel);
         }
-        else{
-            getUzemanyag(benzin)
-            //benzin,elektromos,lpg esetén a benzinhez viszonyul
-        }
-    })
-    $("input, select").change(function(){
-        if(!filled.includes(false)){
-            $("[type = submit]").attr("disabled", false);
+        else {
+            //benzin, elektromos és lpg meghajtású járművek esetén a benzinhez viszonyít
+            getUzemanyag(benzin);
         }
     })
-    function getUzemanyag(t){
-        t.forEach((o)=>{
-            $("#hengerurtartalom").append(`<option value = "${o.value}">${o.text}</option>`)
-            })
-    }
-    //let opt = `<option value = \'1000\'1000 cm3 alatt</option>`;
-    //$("#hengerurtartalom").append(opt);
+    $("[name = amortizaciosDijInput]").change(function(){
+        filled[1] = true;
+        console.log(filled)
+    })
+    $("#arLiterInput").change(function(){
+        egysegar = $(this).val();
+        if (egyegartest.test(egysegar)) {
+                filled[2] = true;
+                console.log(filled)
+        }
+    })
+    $("#cm3Input").change(function(){
+        filled[3] = true;
+        console.log(filled)
+    })
+    $("#rendszmInput").change(function(){
+        rendszam = $(this).val();
+        if (rendszamtest.test(rendszam)) {
+            filled[4] = true;
+            console.log(filled)
+        }
+    })
+    $("#HonnanHovaInput").change(function(){
+        honnanhova = $(this).val();
+        if (!szamtest.test(honnanhova)) {
+            filled[6] = true;
+        }
+    })
+    $("#megtettTInput").change(function(){
+        megtett = $(this).val();
+        if (szamtest.test(megtett)) {
+            filled[7] = true;
+        }
+    })
+
+    $("input, select").change(function () {
+        if (!filled.includes(false)) {
+            $("[type=submit]").attr("disabled", false);
+        }
+    })
+
 })
+
+function getUzemanyag(t) {
+    t.forEach((o) => {
+        $("#cm3Input").append(`<option value="${o.value}">${o.text}</option>`);
+    })
+}
+
+function isFilled() {
+
+}
